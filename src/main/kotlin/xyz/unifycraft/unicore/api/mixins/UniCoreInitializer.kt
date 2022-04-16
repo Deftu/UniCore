bb36daff53aef6ccd3e87b43ab15a7f665e3b671
Deftu <deftudev@gmail.com>
2022-04-16 23:50:42 +0200

    Remove networking and translation API temporarily.

--- src/main/kotlin/xyz/unifycraft/unicore/api/mixins/UniCoreInitializer.kt
+++ src/main/kotlin/xyz/unifycraft/unicore/api/mixins/UniCoreInitializer.kt
@@ -2,10 +2,15 @@ package xyz.unifycraft.unicore.api.mixins
 
 import org.spongepowered.asm.launch.MixinBootstrap
 import org.spongepowered.asm.mixin.Mixins
+import xyz.unifycraft.unicore.api.UniCore
 
 object UniCoreInitializer {
+    private var initialized = false
     @JvmStatic fun initialize() {
+        if (initialized) return
         MixinBootstrap.init()
         Mixins.addConfiguration("mixins.unicore.json")
+        UniCore.getLogger().info("${UniCore.getName()} has been initialized!")
+        initialized = true
     }
 }
--- src/main/kotlin/xyz/unifycraft/unicore/api/utils/language/LanguageFetcher.kt
+++
@@ -1,9 +0,0 @@
-package xyz.unifycraft.unicore.api.utils.language
-
-interface LanguageFetcher {
-    val supportsMultipleFiles: Boolean
-    suspend fun retrieveSingular(path: String): Language? =
-        null
-    suspend fun retrieveMultiple(path: String): List<Language?> =
-        emptyList()
-}
