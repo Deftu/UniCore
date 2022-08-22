9b1f322d6fcfe92bb7c1521c059851629ae67b9a
Deftu <deftudev@gmail.com>
2022-08-22 20:36:18 +0200

    Adapt to new Configured beta.

--- src/main/kotlin/xyz/unifycraft/unicore/api/UniCoreConfig.kt
+++ src/main/kotlin/xyz/unifycraft/unicore/api/UniCoreConfig.kt
@@ -1,11 +1,9 @@
 package xyz.unifycraft.unicore.api
 
 import xyz.unifycraft.configured.Config
-import xyz.unifycraft.configured.data.type.*
 import java.io.File
 
 class UniCoreConfig : Config(
-    file = File(UniCore.getFileHelper().dataDir, "config.json"),
+    directory = File(UniCore.getFileHelper().dataDir, "config"),
     title = UniCore.getName()
-) {
-}
+)
